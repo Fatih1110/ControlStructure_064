ad9dedc483ee6fabc9eaee26925735d227463046
Fatih1110 <vincenrafiq@gmail.com>
2026-09-22 11:43:57 +0700

    Soal 1 if else student permormance

---
+++ ConControlStructure1_064.py
@@ -0,0 +1,13 @@
+
+grade = int(input("Enter the student's percentage: "))
+
+if grade >= 90:
+    print("Excellent performance")
+elif grade >= 80:
+    print("Very Good performance")
+elif grade >= 70:
+    print("Good performance")
+elif grade >= 60:
+    print("Average performance")
+else:
+    print("Bad performance")
